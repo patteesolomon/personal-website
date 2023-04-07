2b43bb081c64375da5c52d7e4be54913e934d82a
patteesolomon <theshiningkillerrashakublade@outlook.com>
2023-04-06 19:17:24 -0500

    Update javascript.js
    JS INSTAL

--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
+var title = document.getElementById("name");
+var words = `Bryan perez`;
+let i = 0;
+for(;i < title.innertext.length ; i++)
+{
+  title.textContent;
+}
+var speed = 100;
+function typeWriter() {
+    if (i < words.length) {
+      title.textContent += words.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+
+typeWriter();
 console.log("Hello World");
